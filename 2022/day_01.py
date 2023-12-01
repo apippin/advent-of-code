cb877d8d24524358f9d0a3e9d6306dfe9c9bc28d
Alan J. Pippin <apippin@pippins.net>
2023-11-30 21:05:13 -0700

    Solved 2022 Day1

--- 2022/day_01.py
+++ 2022/day_01.py
@@ -23,20 +23,31 @@ from aoc_helper import (
 
 raw = aoc_helper.fetch(1, 2022)
 
-
 def parse_raw(raw: str):
-    return ...
+    lines = raw.splitlines()
+    return lines
 
 
 data = parse_raw(raw)
-
 
 # providing this default is somewhat of a hack - there isn't any other way to
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
-
+    calories = []
+    elf = 0
+    calories.append(0)
+    for line in data:
+        #print(f"elf: {elf} line: {line}")
+        if not line.strip():
+            elf += 1
+            calories.append(0)
+        else:
+            calories[elf] += int(line)
+    calories.sort(reverse=True)
+    max_calories = calories[0]
+    print(f"P1: Max calories: {max_calories}")
+    return calories
 
 aoc_helper.lazy_test(day=1, year=2022, parse=parse_raw, solution=part_one)
 
@@ -45,8 +56,9 @@ aoc_helper.lazy_test(day=1, year=2022, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
-
+    calories = part_one(data)
+    max_calories = calories[0]+calories[1]+calories[2]
+    print(f"P2: Max calories: {max_calories}")
 
 aoc_helper.lazy_test(day=1, year=2022, parse=parse_raw, solution=part_two)
 
