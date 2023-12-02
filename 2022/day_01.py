02f71b78a19621cfe794d21d71de847147a0455f
Alan J. Pippin <apippin@pippins.net>
2023-12-02 09:58:57 -0700

    Cleaned up 2022 days and tempalte

--- 2022/day_01.py
+++ 2022/day_01.py
@@ -2,37 +2,36 @@ from collections import defaultdict, deque
 from typing import TYPE_CHECKING
 
 import aoc_helper
-from aoc_helper import (
-    Grid,
-    PrioQueue,
-    SparseGrid,
-    decode_text,
-    extract_ints,
-    extract_iranges,
-    extract_ranges,
-    extract_uints,
-    frange,
-    irange,
-    iter,
-    list,
-    map,
-    range,
-    search,
-    tail_call,
-)
 
+# What day and year is this solution for?
+day = 1
+year = 2022
+
+# Sample Data
+# This list represents the Calories of the food carried by five Elves:
+# 1000
+# 2000
+# 3000
+# 
+# 4000
+#
+# 5000
+# 6000
+#
+# 7000
+# 8000
+# 9000
+# 
+# 10000
 raw = aoc_helper.fetch(1, 2022)
 
 def parse_raw(raw: str):
     lines = raw.splitlines()
     return lines
 
-
 data = parse_raw(raw)
 
-# providing this default is somewhat of a hack - there isn't any other way to
-# force type inference to happen, AFAIK - but this won't work with standard
-# collections (list, set, dict, tuple)
+# Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 def part_one(data=data):
     calories = []
     elf = 0
@@ -49,18 +48,16 @@ def part_one(data=data):
     print(f"P1: Max calories: {max_calories}")
     return calories
 
-aoc_helper.lazy_test(day=1, year=2022, parse=parse_raw, solution=part_one)
-
-
-# providing this default is somewhat of a hack - there isn't any other way to
-# force type inference to happen, AFAIK - but this won't work with standard
-# collections (list, set, dict, tuple)
+# Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 def part_two(data=data):
     calories = part_one(data)
     max_calories = calories[0]+calories[1]+calories[2]
     print(f"P2: Max calories: {max_calories}")
 
-aoc_helper.lazy_test(day=1, year=2022, parse=parse_raw, solution=part_two)
+# Check that the test data then full data works for part_one
+aoc_helper.lazy_test(day=day, year=year, parse=parse_raw, solution=part_one)
+aoc_helper.lazy_submit(day=day, year=year, solution=part_one, data=data)
 
-aoc_helper.lazy_submit(day=1, year=2022, solution=part_one, data=data)
-aoc_helper.lazy_submit(day=1, year=2022, solution=part_two, data=data)
+# Check that the test data then full data works for part_two
+aoc_helper.lazy_test(day=day, year=year, parse=parse_raw, solution=part_two)
+aoc_helper.lazy_submit(day=day, year=year, solution=part_two, data=data)
