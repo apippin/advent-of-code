6f6a0329b750fcefc116cb028c4b5cbd5b1e4dfa
Alan J. Pippin <apippin@pippins.net>
2023-12-01 22:51:41 -0700

    day02 solution

--- 2023/day_02.py
+++ 2023/day_02.py
@@ -21,12 +21,35 @@ from aoc_helper import (
     tail_call,
 )
 
+import re
+import operator
+from functools import reduce
+
+from collections import defaultdict
+def nested_dict(n, type):
+    if n == 1:
+        return defaultdict(type)
+    else:
+        return defaultdict(lambda: nested_dict(n-1, type))
+
+max_cubes = { "red": 12, "green": 13, "blue": 14 }
+
 raw = aoc_helper.fetch(2, 2023)
 
-
 def parse_raw(raw: str):
-    return ...
-
+    lines = raw.splitlines()
+    data = nested_dict(3, int)
+    for line in lines:
+        match = re.search(r"Game (\d+):", line)
+        game = int(match.group(1))
+        line = line.replace(f"Game {game}: ","")
+        game_sets = line.split(";")
+        for set_num, game_set in enumerate(game_sets):
+            cubes = game_set.split(",")
+            for cube in cubes:
+                match = re.search(r"(\d+) (\w+)", cube)
+                data[game][set_num][match.group(2)] = int(match.group(1))
+    return data
 
 data = parse_raw(raw)
 
@@ -35,8 +58,17 @@ data = parse_raw(raw)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
-
+    sum_game_ids = 0
+    for game in data.keys():
+        game_possible=True
+        for set_num in data[game].keys():
+            for color in data[game][set_num].keys():
+                print(f"game: {game} set: {set_num} color: {color} num: {data[game][set_num][color]}")
+                if data[game][set_num][color] > max_cubes[color]:
+                    game_possible=False
+        if game_possible:
+            sum_game_ids += game
+    return sum_game_ids
 
 aoc_helper.lazy_test(day=2, year=2023, parse=parse_raw, solution=part_one)
 
@@ -45,7 +77,16 @@ aoc_helper.lazy_test(day=2, year=2023, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    sum_game_cube_powers = 0
+    for game in data.keys():
+        game_min_cubes = { "red": 0, "green": 0, "blue": 0 }
+        for set_num in data[game].keys():
+            for color in data[game][set_num].keys():
+                print(f"game: {game} set: {set_num} color: {color} num: {data[game][set_num][color]}")
+                if data[game][set_num][color] > game_min_cubes[color]:
+                    game_min_cubes[color] = data[game][set_num][color]
+        sum_game_cube_powers += reduce(operator.mul, list(game_min_cubes.values()), 1)
+    return sum_game_cube_powers
 
 
 aoc_helper.lazy_test(day=2, year=2023, parse=parse_raw, solution=part_two)
