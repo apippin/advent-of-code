8360f374f9622bf0b0972c7eb0cf6022f01a18e0
Alan J. Pippin <apippin@pippins.net>
2023-12-07 23:34:11 -0700

    day_08 solution part2

--- 2023/day_08.py
+++ 2023/day_08.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 import aoc_helper
 
 import re
+import itertools as it
+from math import lcm
 
 # What day and year is this solution for?
 day = 8
@@ -20,7 +22,7 @@ year = 2023
 # ZZZ = (ZZZ, ZZZ)
 raw = aoc_helper.fetch(day, year)
 
-# Parse out the steps and nodes
+# Parse out the steps and nodes and store them in a dictionary
 def parse_raw(raw: str):
     lines = raw.splitlines()
     steps = list(lines[0])
@@ -34,48 +36,42 @@ def parse_raw(raw: str):
 # Parse the game data
 data = parse_raw(raw)
 
+# Find the number of steps to get from the given node to an ending node
+def path_finder(data, node):
+    num_steps = 0
+    #print(f"starting from {node}")
+    for step in it.cycle(data['steps']):
+        #print(f"step: {step}")
+        num_steps += 1
+        match step:
+            case 'L':
+                node = data[node][0]
+            case 'R':
+                node = data[node][1]
+        #print(f"next_node: {node}")
+        if node.endswith('Z'):
+            #print(f"found path with num_steps: {num_steps}")
+            return num_steps
+
 # Starting at AAA, follow the left/right instructions.
 # How many steps are required to reach ZZZ?
 def part_one(data=data):
     #print(f"{data}")
-    node = 'AAA'
-    num_steps = 0
-    while(node != 'ZZZ'):
-        for step in data["steps"]:
-            match step:
-                case 'L':
-                    node = data[node][0]
-                case 'R':
-                    node = data[node][1]
-            #print(f"next_step: {node}")
-            num_steps += 1
+    num_steps = path_finder(data, 'AAA')
     print(f"num_steps: {num_steps}")
     return num_steps
 
 # Simultaneously start on every node that ends with A.
 # How many steps does it take before you're only on nodes that end with Z?
-def path_finder(node):
-    pass
-
 def part_two(data=data):
-    #print(f"{data}")
-    num_steps = 0
-    nodes = [x for x in data.keys() if x.endswith('A')]
-    #print(f"starting_nodes: {nodes}")
-    while(len([x for x in nodes if x.endswith('Z')]) != len(nodes)):
-        for step in data['steps']:
-            #print(f"step: {step}")
-            new_nodes = list()
-            for node in nodes:
-                #print(f"node: {node}")
-                match step:
-                    case 'L':
-                        new_nodes.append(data[node][0])
-                    case 'R':
-                        new_nodes.append(data[node][1])
-            num_steps += 1
-            nodes = new_nodes
-            #print(f"next_step: {nodes}")
+    starting_nodes = [x for x in data.keys() if x.endswith('A')]
+    #print(f"{starting_nodes}")
+    # Paths are cyclic so navigating them all together takes a really long time (mine never finished).
+    # So let's do this mathematically by first finding the steps per path to each end node, and then taking the LCM of those.
+    # The LCM will be the number of steps for each path to get to an end node, because it is the least common mulitple of each path length.
+    # The LCM is the lowest number that is a multiple of each path's steps to get to a node ending in Z.
+    lcms = [path_finder(data, node) for node in starting_nodes]
+    num_steps = lcm(*lcms)
     print(f"num_steps: {num_steps}")
     return num_steps
 
