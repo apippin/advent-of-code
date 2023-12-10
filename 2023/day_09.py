d4bb75ea32d200100910b1ac23b3c4666a955e19
Alan J. Pippin <apippin@pippins.net>
2023-12-09 23:44:40 -0700

    Cleaned up day_09 solution

--- 2023/day_09.py
+++ 2023/day_09.py
@@ -29,29 +29,39 @@ def parse_raw(raw: str):
 # Parse the game data
 data = parse_raw(raw)
 
-# Analyze your OASIS report and extrapolate the next value for each history.
-def part_one(data=data):
-    #print(f"{data}")
+# diff each element in the history of values to find the next value in the list
+def extrapolate(data, part_two=False):
     for hist in data:
+        if part_two:
+            hist.reverse()
         diffs = [hist]
         while any(diffs[-1]):
             diffs.append(np.diff(diffs[-1]))
         #print(*diffs,sep='\n')
         next_val = sum(val[-1] for val in diffs)
         hist.append(next_val)
-    #print(*data,sep='\n')
+
+# Analyze your OASIS report and extrapolate the next value for each history.
+def part_one(data=data):
+    #print(f"{data}")
+    extrapolate(data)
     next_val_sum = sum(vals[-1] for vals in data)
     print(f"next_val_sum: {next_val_sum}")
     return next_val_sum
 
-# ...
+# Analyze your OASIS report again, this time extrapolating the previous value for each history.
+# What is the sum of these extrapolated values?
 def part_two(data=data):
-    ...
+    #print(f"{data}")
+    extrapolate(data, True)
+    next_val_sum = sum(vals[-1] for vals in data)
+    print(f"next_val_sum: {next_val_sum}")
+    return next_val_sum
 
 # Check that the test data then full data works for part_one
-#aoc_helper.lazy_test(day=day, year=year, parse=parse_raw, solution=part_one)
-#aoc_helper.lazy_submit(day=day, year=year, solution=part_one, data=data)
+aoc_helper.lazy_test(day=day, year=year, parse=parse_raw, solution=part_one)
+aoc_helper.lazy_submit(day=day, year=year, solution=part_one, data=data)
 
 # Check that the test data then full data works for part_two
 aoc_helper.lazy_test(day=day, year=year, parse=parse_raw, solution=part_two)
-#aoc_helper.lazy_submit(day=day, year=year, solution=part_two, data=data)
+aoc_helper.lazy_submit(day=day, year=year, solution=part_two, data=data)
