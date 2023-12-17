c37fba166b1ca30b4f2cab33af13826969d54ad4
Alan J. Pippin <apippin@pippins.net>
2023-12-16 22:44:50 -0700

    day_15 part2 solution

--- 2023/day_15.py
+++ 2023/day_15.py
@@ -19,6 +19,22 @@ def parse_raw(raw: str):
 # Parse the game data
 data = parse_raw(raw)
 
+# Run the HASH algorithm on the given step
+def get_hash_value(data, step):
+    # For each character in each step
+    current_value = 0
+    for ch in step:
+        # Convert the character into its ASCII code
+        ascii_code = ord(ch)
+        # Increase current_value by its ASCII code
+        current_value += ascii_code
+        # Set the current_value to itself multiplied by 17
+        current_value *= 17
+        # Set the current value to the remainder of dividing itself by 256
+        current_value %= 256
+        #print(f"{s}: {ch} {current_value}")
+    return current_value
+
 # Run the HASH algorithm on each step in the initialization sequence.
 # What is the sum of the results? (The initialization sequence is one long line; be careful when copy-pasting it.)
 def part_one(data=data):
@@ -26,22 +42,9 @@ def part_one(data=data):
     # For each step
     sum_of_results = 0
     for s,step in enumerate(data):
-        # For each character in each step
-        current_value = 0
-        for ch in step:
-            # Convert the character into its ASCII code
-            ascii_code = ord(ch)
-            # Increase current_value by its ASCII code
-            current_value += ascii_code
-            # Set the current_value to itself multiplied by 17
-            current_value *= 17
-            # Set the current value to the remainder of dividing itself by 256
-            current_value %= 256
-            #print(f"{s}: {ch} {current_value}")
-        # Tally our results
+        current_value = get_hash_value(data, step)
         #print(f"{s}: {step} {current_value}")
         sum_of_results += current_value
-
     print(f"sum_of_results: {sum_of_results}")
     return sum_of_results
 
@@ -49,12 +52,48 @@ def part_one(data=data):
 # follow the initialization sequence.
 # What is the focusing power of the resulting lens configuration?
 def part_two(data=data):
-    ...
+    #print(*data, sep="\n")
+    boxes = {}
+    for step in data:
+        match = re.search('(\w+)([=-]+)(\d*)',step)
+        label = match[1]
+        box = get_hash_value(data, label)
+        operation = match[2]
+        focal_length = match[3]
+        #print(f"step: {step} box: {box} operation: {operation} focal_length: {focal_length}")
+        if operation == '=':
+            if box not in boxes:
+                boxes[box] = [[label, focal_length]]
+                #print(f"={boxes}")
+            else:
+                for label_in_box in boxes[box]:
+                    if label == label_in_box[0]:
+                        #print(f"label {label} in box {box}")
+                        label_in_box[1] = focal_length
+                        break
+                else:
+                    boxes[box].append([label, focal_length])
+                #print(f"+{boxes}")
+        elif box in boxes and operation == '-':
+            boxes[box] = [[val, key] for (val, key) in boxes[box] if val != label]
+            #print(f"-{boxes}")
+    print(f"{boxes}")
+
+    # To confirm that all of the lenses are installed correctly, add up the focusing power of all of the lenses.
+    sum_of_focusing_power = 0
+    for box in boxes:
+        for slot,lens in enumerate(boxes[box],1):
+            #print(f"box: {box} lens: {lens[0]} slot: {slot} focal_length: {lens[1]}")
+            focusing_power = (1 + box) * (slot) * (int(lens[1]))
+            #print(f"box: {box} lens: {lens[0]} slot: {slot} focal_length: {lens[1]} focusing_power: {focusing_power}")
+            sum_of_focusing_power += int(focusing_power)
+    print(f"sum_of_focusing_power: {sum_of_focusing_power}")
+    return sum_of_focusing_power
 
 # Check that the test data then full data works for part_one
-#aoc_helper.lazy_test(day=day, year=year, parse=parse_raw, solution=part_one)
-#aoc_helper.lazy_submit(day=day, year=year, solution=part_one, data=data)
+aoc_helper.lazy_test(day=day, year=year, parse=parse_raw, solution=part_one)
+aoc_helper.lazy_submit(day=day, year=year, solution=part_one, data=data)
 
 # Check that the test data then full data works for part_two
 aoc_helper.lazy_test(day=day, year=year, parse=parse_raw, solution=part_two)
-#aoc_helper.lazy_submit(day=day, year=year, solution=part_two, data=data)
+aoc_helper.lazy_submit(day=day, year=year, solution=part_two, data=data)
